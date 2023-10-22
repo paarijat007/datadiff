1cf1cfe1cac75cc2e13b9b5c2649a913b66f19e9
Paarijat <paarijat@pop-os.localdomain>
2023-10-22 09:31:17 +0530

    started diff tool

---
+++ compare.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+"database/sql"
+"fmt"
+"log"
+
+_"github.com/lib/pq"
+)
+
+func main() {
+diff := compareTables()
+
+if len(diff) == {
+fmt.Println("The tables are identical.")
+}
+
+
---
+++ go.mod
@@ -0,0 +1,3 @@
+module datadiff
+
+go 1.21.3
